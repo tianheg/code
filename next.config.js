686c7d1778c409172f6d2d5dd3a11176cd0b00ec
tianheg <yidajiabei@gmail.com>
2023-02-21 02:02:02 +0800

    GitHub Action with Next.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.jsx',
-  defaultShowCopyCode: true
+  defaultShowCopyCode: true,
 })
- 
+
 module.exports = withNextra()
- 
+
 // If you have other Next.js configurations, you can pass them as the parameter:
-// module.exports = withNextra({ /* other next.js config */ })
+module.exports = withNextra({ images: { unoptimized: true, } })
