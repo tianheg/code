f9d90fd810c362fca72c552be15622b4b09574c8
tianheg <yidajiabei@gmail.com>
2023-02-21 22:59:54 +0800

    Update title,footer

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,11 +1,17 @@
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  logo: <span>Codes | 一大加贝</span>,
+  logo: <span>Codes</span>,
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – 一大加贝'
+    }
+  },
   project: {
     link: 'https://github.com/tianheg/codes',
   },
   docsRepositoryBase: 'https://github.com/tianheg/codes',
   footer: {
-    text: '',
+    text: '每天进步一点点',
   },
   feedback: {content: null},
   navigation: false
