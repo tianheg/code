d67dd4b55326e98bf416ce355c0694b0628cee97
tianheg <yidajiabei@gmail.com>
2023-03-04 12:01:38 +0800

    Update theme.config.jsx

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  logo: <span>Codes</span>,
+  logo: <span>Code</span>,
   useNextSeoProps() {
     return {
       titleTemplate: '%s – 一大加贝'
