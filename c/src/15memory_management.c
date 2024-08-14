8cb9e17e0d765bc198da0e8424cc68a9adfe0ee0
tianheg <i@tianheg.co>
2024-08-14 22:57:14 +0800

    update c memory_management

--- c/src/15memory_management.c
+++ c/src/15memory_management.c
@@ -1,12 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-  int *ptr1 = malloc(4);
-  char *ptr2 = (char*) ptr1;
-  ptr1[0] = 1684234849;
+struct list {
+  int *data;
+  int numItems;
+  int size;
+};
 
-  printf("%d is %c %c %c %c\n", *ptr1, ptr2[0], ptr2[1], ptr2[2], ptr2[3]);
-  
+void addToList(struct list *myList, int item);
+
+int main() {
+  struct list myList;
+  int amount;
+
+  myList.numItems = 0;
+  myList.size = 10;
+  myList.data = malloc(myList.size * sizeof(int));
+
+  if (myList.data == NULL) {
+    printf("Memory allocation failed");
+    return 1;
+  }
+
+  amount = 44;
+
+  for (int i  = 0;i < amount; i++) {
+    addToList(&myList, i+1);
+  }
+  for (int j = 0;j<myList.numItems;j++) {
+    printf("%d ", myList.data[j]);
+  }
+
+  free(myList.data);
+  myList.data = NULL;
+
   return 0;
+}
+
+void addToList(struct list *myList, int item) {
+  if(myList->numItems == myList->size) {
+    myList->size += 10;
+    myList->data = realloc(myList->data, myList->size * sizeof(int));
+  }
+
+  myList->data[myList->numItems] = item;
+  myList->numItems++;
 }
